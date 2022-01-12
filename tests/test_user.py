6f22e291d61c2e616ab9e0415ffb2f2caacea59f
Patrick O'Connor <poc.967@gmail.com>
2022-01-11 22:37:30 -0500

    more user tests

--- tests/test_user.py
+++ tests/test_user.py
@@ -22,6 +22,12 @@ class UserTestSuite(unittest.TestCase):
             "password": "supersecret"
         }
 
+        self.invalid_user_data = {
+            "first_name": "Test",
+            "last_name": "McTest",
+            "password": "supersecret"
+        }
+
         return
 
     def test_create_user_success(self):
@@ -33,7 +39,7 @@ class UserTestSuite(unittest.TestCase):
                 (data, status) = response
                 self.assertEqual(status, 201)
 
-    def test_create_user_endpoint(self):
+    def test_get_user_endpoint(self):
         with self.app.test_client() as c:
             new_user = user.User().save()
             response = c.get(f'/api/users/{new_user.id}')
@@ -42,6 +48,25 @@ class UserTestSuite(unittest.TestCase):
 
             self.assertEqual(status, '200 OK')
             self.assertIsNotNone(data)
+
+    def test_create_user_endpoint_valid_data(self):
+        with self.app.test_client() as c:
+            response = c.post('/api/users', data=self.valid_user_data)
+            status = response.status
+            data = response.data.decode('utf-8')
+            data = json.loads(data)
+
+            self.assertEqual(status, '201 CREATED')
+            self.assertIsNotNone(data)
+
+    def test_create_user_endpoint_invalid_data(self):
+        with self.app.test_client() as c:
+            response = c.post('/api/users', data=self.invalid_user_data)
+            status = response.status
+            data = response.data.decode('utf-8')
+            data = json.loads(data)
+
+            self.assertEqual(status, '400 BAD REQUEST')
 
     def tearDown(self):
         user.User.drop_collection()
